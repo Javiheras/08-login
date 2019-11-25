5c9f57c4790cba6bdcebeb6a61b5d22f0c72b4b4
Javier <javier@onesolutions.es>
2019-11-25 18:58:56 +0100

    Página login

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AuthenticationService } from '../services/authentication.service';
+import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  validations_form: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+ 
+  validation_messages = {
+   'email': [
+     { type: 'required', message: 'El correo el obligatorio.' },
+     { type: 'pattern', message: 'Introduce correo válido.' }
+   ],
+   'password': [
+     { type: 'required', message: 'Cobtraseña obligatoria.' },
+     { type: 'minlength', message: 'La contraseña debe tener 6 caracetres.' }
+   ]
+ };
+ 
+  constructor(
+    private navCtrl: NavController,
+    private authService: AuthenticationService,
+    private formBuilder: FormBuilder
+  ) {}
+ 
+  ngOnInit(){
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
+  }
+ 
+  tryRegister(value){
+    this.authService.registerUser(value)
+     .then(res => {
+       console.log(res);
+       this.errorMessage = "";
+       this.successMessage = "La cuenta ha sido creada.";
+     }, err => {
+       console.log(err);
+       this.errorMessage = err.message;
+       this.successMessage = "";
+     })
+  }
+ 
+  goLoginPage(){
+    this.navCtrl.navigateBack('');
   }
 
 }
