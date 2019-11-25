3ae5a14070f2ca25a4975d52534ed66c8baa04fa
Javier <javier@onesolutions.es>
2019-11-25 18:26:45 +0100

    Import

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCecBu5fWorUS2mWcyGQ3jiif92FXU6uU8",
+    authDomain: "fir-login-4b54d.firebaseapp.com",
+    databaseURL: "https://fir-login-4b54d.firebaseio.com",
+    projectId: "fir-login-4b54d",
+    storageBucket: "fir-login-4b54d.appspot.com",
+    messagingSenderId: "546940012038",
+    appId: "1:546940012038:web:d3c2388bcdc63f39eea587"
+  }
 };
 
 /*
